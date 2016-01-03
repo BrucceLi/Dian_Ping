bda68cc8e596695d9bff3a47bcc66b2a8c5608d1
BrucceLi <unity_unity@163.com>
2016-01-03 09:51:01 +0800

    Update

--- app/src/main/java/com/dianping/activity/AllCityActivity.java
+++ app/src/main/java/com/dianping/activity/AllCityActivity.java
@@ -1,19 +1,40 @@
 package com.dianping.activity;
 
+import android.graphics.Rect;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.dianping.R;
+import com.dianping.adapter.CityRecyclerAdapter;
 import com.dianping.events.CitySelectedEvent;
 import com.dianping.events.LetterCheckedEvent;
 
 import com.dianping.fragment.*;
+import com.dianping.frameworkutils.Data;
+import com.dianping.frameworkutils.SharedUtils;
+import com.dianping.frameworkutils.UTF_8StringRequest;
+import com.dianping.frameworkutils.VolleySingleTone;
+import com.dianping.model.City;
+import com.dianping.model.ResponseWrapper;
 import com.dianping.view.SiderMenu;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.greenrobot.event.EventBus;
 
@@ -24,17 +45,76 @@ public class AllCityActivity extends BaseActivity implements SiderMenu.OnLetterC
 	Button btn_backpress;
 	@ViewById
 	SiderMenu sm_selector;
+
+
 	@ViewById
-	TextView tv_show_city_key;
+	RecyclerView recyclerview_city;
+
+	List<City> mList= new ArrayList<>();
+	LinearLayoutManager layoutManager;
+	CityRecyclerAdapter adapter;
 
 	@Override
 	protected void initVariables() {
 
-		getSupportFragmentManager().beginTransaction()
-				.add(R.id.fragment_container, new CityFragment_()).commit();
 
+		configRecyclerView();
 		sm_selector.setOnLetterCheckedListener(this);
+		loadCities();
+
+
+
 	}
+
+	void configRecyclerView()
+	{
+		layoutManager = SharedUtils.getVerticalLinearLayoutManager(this);
+		recyclerview_city.setLayoutManager(layoutManager);
+		recyclerview_city.addItemDecoration(new RecyclerView.ItemDecoration() {
+			@Override
+			public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+				outRect.set(1,1,1,1);
+
+			}
+		});
+	}
+
+
+	void loadCities()
+	{
+		UTF_8StringRequest stringRequest=new UTF_8StringRequest(Request.Method.GET, Data.CITY_URI , new Response.Listener<String>() {
+			@Override
+			public void onResponse(String s) {
+				try {
+
+					Gson gson = new Gson();
+					ResponseWrapper<List<City>> result=gson.fromJson(s, new TypeToken<ResponseWrapper<List<City>>>(){}.getType());
+					if(result.getResult_State()==1)
+					{
+						mList = result.getData();
+						adapter = new CityRecyclerAdapter(mList);
+						recyclerview_city.setAdapter(adapter);
+
+					}
+					else
+					{
+						showToast("无数据");
+
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}, new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError volleyError) {
+				showToast("检查网络！");
+			}
+		});
+		VolleySingleTone.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+
+	}
+
 	@Click
 	void btn_backpressClicked() {
 
@@ -50,22 +130,19 @@ public class AllCityActivity extends BaseActivity implements SiderMenu.OnLetterC
 
 	@Override
 	public void onLetterChecked(String letter) {
-		if(letter.equals(""))
-		{
-			tv_show_city_key.setVisibility(View.GONE);
-		}else
-		{
-			tv_show_city_key.setVisibility(View.VISIBLE);
-			tv_show_city_key.bringToFront();
-			tv_show_city_key.setText(letter);
+		for (int i = 0; i < mList.size(); i++) {
+			if (letter.equals(mList.get(i).getShotKey())) {
+				layoutManager.scrollToPositionWithOffset(i, 0);
+				break;
+			}
 		}
-		EventBus.getDefault().post(new LetterCheckedEvent(letter));
-
 	}
-
-	//得到城市选定的事件
+	@UiThread(delay = 300)
 	public void onEvent(CitySelectedEvent event)
 	{
 		HomePageActivity_.intent(this).mCityName(event.cityName).start();
 	}
+
+
+
 }
